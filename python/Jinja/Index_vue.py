b81548ab05ff0c086ee65786788dd2145b73f198
thegostisdead <thegostisdead@gmail.com>
2019-12-08 15:24:35 +0100

    nothing

--- python/Jinja/Index_vue.py
+++ python/Jinja/Index_vue.py
@@ -25,28 +25,12 @@ def creer_html(fichier_template, fichier_sortie,**infos):
 
 
 # Ajouter ici les appels à la fonction creer_html
+
+
+
 creer_html("Pagination_0_template.html","./Pages_Genere/page-0.html",
             liste_images = liste_des_images(dico_infos_pages,0),
             navigation_page = navigation_page(dico_infos_pages,0),
             page_agenere = 0,
             )
-creer_html("Pagination_0_template.html","./Pages_Genere/page-1.html",
-            liste_images = liste_des_images(dico_infos_pages,1),
-            navigation_page = navigation_page(dico_infos_pages,1),
-            page_agenere = 1,
-            )
-creer_html("Pagination_0_template.html","./Pages_Genere/page-2.html",
-            liste_images = liste_des_images(dico_infos_pages,2),
-            navigation_page = navigation_page(dico_infos_pages,2),
-            page_agenere = 2,
-            )
-creer_html("Pagination_0_template.html","./Pages_Genere/page-3.html",
-            liste_images = liste_des_images(dico_infos_pages,3),
-            navigation_page = navigation_page(dico_infos_pages,3),
-            page_agenere = 3,
-            )
-creer_html("Pagination_0_template.html","./Pages_Genere/page-4.html",
-            liste_images = liste_des_images(dico_infos_pages,4),
-            navigation_page = navigation_page(dico_infos_pages,4),
-            page_agenere = 4,
-            )
+
