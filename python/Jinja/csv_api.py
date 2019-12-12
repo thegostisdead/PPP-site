ed1c6fe0c8e6b9b763470aa290bb1b754d3b93d8
dorian hardy <thegostisdead@gmail.com>
2019-12-12 16:43:49 +0100

    Creating python csv API

--- python/Jinja/csv_api.py
+++ python/Jinja/csv_api.py
@@ -1,41 +1,40 @@
 import csv
-csv_file = None 
-with open('output.csv', mode='r') as csv_file:
-    csv_file = csv.DictReader(csv_file)
-    
 
 
-def readCollum(collum_name,csv_file):
+
+def readCollum(collum_name,csv_filename):
     """
     parametres : collum_name : la collone a lire 
                  csv_file : le fichier csv 
     resultat : le text des collones 
 
     """
-    res = []
-    line_count = 0
-    for row in csv_file:
-        if line_count == 0:
-            line_count += 1
-        res.append([str(collum_name)])
-        line_count += 1
-    return res
-    
 
+    with open(csv_filename, mode='r') as csv_file:
+        res = []
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            res.append(row[collum_name])
+            line_count += 1
+        return res
+       
 
 def get_all_name_jobs():
     """
     parametres :
     resultat : le nom de tout les métiers
     """
-    return readCollum("name", csv_file)
+    return readCollum("name", "output.csv")
     
 def get_all_level_of_studies():
     """
     parametres :
     resultat : le nom de tout les métiers
     """
-    return readCollum("level", csv_file)
+    return readCollum("level", "output.csv")
     
 
 def get_all_description():
@@ -43,25 +42,25 @@ def get_all_description():
     parametres :
     resultat : le nom de tout les métiers
     """
-    return readCollum("description", csv_file)
+    return readCollum("description", "output.csv")
 
 def get_all_category():
     """
     parametres :
     resultat : le nom de tout les métiers
     """
-    return readCollum("category", csv_file)
+    return readCollum("category", "output.csv")
 
 def get_all_money():
     """
     parametres :
     resultat : le nom de tout les métiers
     """
-    return readCollum("money", csv_file)
+    return readCollum("money", "output.csv")
 
 def get_all_link():
     """
     parametres :
     resultat : le nom de tout les métiers
     """
-    return readCollum("link", csv_file)
+    return readCollum("link", "output.csv")
