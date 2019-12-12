59f598fd72b44d99d18b5d28ed5b854a70ec1d17
dorian hardy <thegostisdead@gmail.com>
2019-12-12 15:59:43 +0100

    modif

--- python/Jinja/modele.py
+++ python/Jinja/modele.py
@@ -1,4 +1,35 @@
 # modele_Index.py
 
+import csv 
 
 
+with open('output.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        print(f'\t{row["name"]}.')
+        line_count += 1
+    print(f'Processed {line_count} lines.')
+
+
+
+
+
+
+
+
+
+
+# traitement 
+
+
+def readCollum(collum_name):
+    pass
+
+
+def get_name_jobs(csv_file):
+    pass
+    
