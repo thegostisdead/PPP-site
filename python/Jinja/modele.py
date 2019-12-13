ee5f754ceceb4f66013644e42ab61217495d42e1
IronJulo <broser.julo@laposte.net>
2019-12-14 00:42:52 +0100

    jinja de la page aide et css propre pour la meme page manque les images propres

--- python/Jinja/modele.py
+++ python/Jinja/modele.py
@@ -3,10 +3,10 @@
 import csv_api
 
 liens_importants = [
-                    ('Onisep',"L’Onisep a pour vocation d’informer sur les formations, les métiers, les secteurs professionnels. Il guide les jeunes et leur famille dans leurs choix de parcours de formation et de projet professionnel.",'http://www.onisep.fr/'),
-                    ('Studyrama',"En tant qu’acteur référent du monde de l’orientation et de l’aide à la réussite aux examens et aux concours, notre vocation est d’accompagner et de conseiller chaque individu tout au long de son parcours étudiant et professionnel.",'http://www.studyrama.com'),
-                    ('Diplomeo',"Diplomeo est un des principaux moteurs de recherche de formations en France. Il recense plus de 40 000 formations ciblant les étudiants.C'est un outil rassemblant en un seul endroit toutes les informations concernant l’enseignement supérieur",'https://diplomeo.com/'),
-                    ('Imaginetonfutur',"Sur imaginetonfutur retrouvez plus de 600 fiches métiers et des infos pratiques sur votre futur diplome pour trouver le métier vous convient.",'https://www.imaginetonfutur.com/'),('','','')
+                    ('Onisep',"L’Onisep a pour vocation d’informer sur les formations, les métiers, les secteurs professionnels. Il guide les jeunes et leur famille dans leurs choix de parcours de formation et de projet professionnel.",'http://www.onisep.fr/','../../../img/onisep.png'),
+                    ('Studyrama',"En tant qu’acteur référent du monde de l’orientation et de l’aide à la réussite aux examens et aux concours, notre vocation est d’accompagner et de conseiller chaque individu tout au long de son parcours étudiant et professionnel.",'http://www.studyrama.com','../../../img/onisep.png'),
+                    ('Diplomeo',"Diplomeo est un des principaux moteurs de recherche de formations en France. Il recense plus de 40 000 formations ciblant les étudiants.C'est un outil rassemblant en un seul endroit toutes les informations concernant l’enseignement supérieur",'https://diplomeo.com/','../../../img/onisep.png'),
+                    ('Imaginetonfutur',"Sur imaginetonfutur retrouvez plus de 600 fiches métiers et des infos pratiques sur votre futur diplome pour trouver le métier vous convient.",'https://www.imaginetonfutur.com/','../../../img/onisep.png'),
                     ]
 
 
@@ -23,34 +23,34 @@ etudiants = {
 # traitement 
 
 
-def get_student(dico):
-    """
-    params : 
-    retourn : 
-    """
-    res = []
-    for student_name, (profile_picture, description) in dico.items():
-        res.append(student_name,profile_picture,description)
+# def get_student(dico):
+#     """
+#     params : 
+#     retourn : 
+#     """
+#     res = []
+#     for student_name, (profile_picture, description) in dico.items():
+#         res.append(student_name,profile_picture,description)
 
-    return res 
+#     return res 
 
     
-def get_categories():
-    """
-    params : 
-    retourne : 
-    """
-    category = set()
-    for elem in csv_api.get_all_category():
-        category.add(elem)
-    return category
+# def get_categories():
+#     """
+#     params : 
+#     retourne : 
+#     """
+#     category = set()
+#     for elem in csv_api.get_all_category():
+#         category.add(elem)
+#     return category
 
-def get_levels():
-    """
-    paramètre : 
-    retourne : un ensemble qui contient 
-    """
-    levels = set()
-    for elem in csv_api.get_all_level_of_studies():
-        levels.add(elem)
-    return levels
+# def get_levels():
+#     """
+#     paramètre : 
+#     retourne : un ensemble qui contient 
+#     """
+#     levels = set()
+#     for elem in csv_api.get_all_level_of_studies():
+#         levels.add(elem)
+#     return levels
--- python/Jinja/vue.py
+++ python/Jinja/vue.py
@@ -28,25 +28,25 @@ def creer_html(fichier_template, fichier_sortie,**infos):
 
 
 
-creer_html("template_index.html","pages/index.html", #GENERATION DE LA PAGE INDEX
-    categories = get_categories(),
-    niveaux = get_levels()        
+# creer_html("template_index.html","pages/index.html", #GENERATION DE LA PAGE INDEX
+#     categories = get_categories(),
+#     niveaux = get_levels()        
+#             )
+
+# creer_html("template_a_propos.html","pages/a_propos.html", #GENERATION DE LA PAGE A_PROPOS
+#     groupe = get_student(etudiants)
+#     )
+
+creer_html("template_aide.html","./pages/aide.html", #GENERATION DE LA PAGE AIDE
+            liens_importants = liens_importants
             )
 
-creer_html("template_a_propos.html","pages/a_propos.html", #GENERATION DE LA PAGE A_PROPOS
-    groupe = get_student(etudiants)
-    )
-
-""" creer_html("template_aide.html","pages/index.html", #GENERATION DE LA PAGE AIDE
-            
-            )
-
-creer_html("template_categories.html","pages/index.html", #GENERATION DE LA PAGE CATEGORIES
+# creer_html("template_categories.html","pages/index.html", #GENERATION DE LA PAGE CATEGORIES
     
             
-            )
+#             )
 
-creer_html("template_recherches.html","pages/index.html", #GENERATION DE LA PAGE RECHERCHES
+# creer_html("template_recherches.html","pages/index.html", #GENERATION DE LA PAGE RECHERCHES
             
-            ) 
-"""
+#             ) 
+
