16dc294cd307591ac985f8abb201539b815accd1
Jules Brossier <jbrossier@info23-26.iut45.univ-orleans.fr>
2019-12-20 16:31:02 +0100

    reorganisation

--- python/modele.py
+++ python/modele.py
@@ -3,15 +3,12 @@
 import csv_api
 
 liens_importants = [
-                    ('Onisep',"L’Onisep a pour vocation d’informer sur les formations, les métiers, les secteurs professionnels. Il guide les jeunes et leur famille dans leurs choix de parcours de formation et de projet professionnel.",'http://www.onisep.fr/','../../../img/onisep.png'),
-                    ('Studyrama',"En tant qu’acteur référent du monde de l’orientation et de l’aide à la réussite aux examens et aux concours, notre vocation est d’accompagner et de conseiller chaque individu tout au long de son parcours étudiant et professionnel.",'http://www.studyrama.com','../../../img/onisep.png'),
-                    ('Diplomeo',"Diplomeo est un des principaux moteurs de recherche de formations en France. Il recense plus de 40 000 formations ciblant les étudiants.C'est un outil rassemblant en un seul endroit toutes les informations concernant l’enseignement supérieur",'https://diplomeo.com/','../../../img/onisep.png'),
-                    ('Imaginetonfutur',"Sur imaginetonfutur retrouvez plus de 600 fiches métiers et des infos pratiques sur votre futur diplome pour trouver le métier vous convient.",'https://www.imaginetonfutur.com/','../../../img/onisep.png'),
+                    ('Onisep',"L’Onisep a pour vocation d’informer sur les formations, les métiers, les secteurs professionnels. Il guide les jeunes et leur famille dans leurs choix de parcours de formation et de projet professionnel.",'http://www.onisep.fr/','../img/onisep.png'),
+                    ('Studyrama',"En tant qu’acteur référent du monde de l’orientation et de l’aide à la réussite aux examens et aux concours, notre vocation est d’accompagner et de conseiller chaque individu tout au long de son parcours étudiant et professionnel.",'http://www.studyrama.com','../img/onisep.png'),
+                    ('Diplomeo',"Diplomeo est un des principaux moteurs de recherche de formations en France. Il recense plus de 40 000 formations ciblant les étudiants.C'est un outil rassemblant en un seul endroit toutes les informations concernant l’enseignement supérieur",'https://diplomeo.com/','../img/onisep.png'),
+                    ('Imaginetonfutur',"Sur imaginetonfutur retrouvez plus de 600 fiches métiers et des infos pratiques sur votre futur diplome pour trouver le métier vous convient.",'https://www.imaginetonfutur.com/','../img/onisep.png'),
                     ]
-
-
-                  
-                    
+                 
 etudiants = {
     "Dorian Hardy": [("img","Etudiant en 1ère année en DUT informatique.")],
     "Jules Brossier": [("img","Etudiant en 1ère année en DUT informatique.")],
